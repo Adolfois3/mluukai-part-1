e95c8f6973f0aaa0e379f53e01fe6cda98872f01
Adolfois3 <adolfito241107@gmail.com>
2025-07-08 16:54:11 -0400

    Ejercicio 1.14 realizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,47 @@
 import { useState } from "react"
 
 
-const Statistic = ({props, name})=>{
-
-  return(
-    <>
-    <p>{name} {props}</p>
-    </>
-  )
-
-}
-
-const Button = ({name,incrementar})=>{
-
-  return(
-    <button onClick={incrementar}>{name}</button>
-
-  )
-}
 
 export const App =  () => {
+   const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  
 
 
+  const [selected, setSelected] = useState(0)
+    const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
 
 
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [all , setAll]= useState(0)
-
-  function goodIn (){
-    setGood(good + 1)
-        setAll(all + 1)
+  function nextAnecdote (){
+    const randomIndex = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomIndex)
+    
+    
   }
-    function neutralIn (){
-    setNeutral(neutral + 1)
-        setAll(all + 1)
-  }
-
-    function badIn (){
-    setBad(bad + 1)
-    setAll(all  + 1 )
-  }
-
-
-    const promedio = good + neutral + bad / all
-    const promedioPositivo = good / all * 100
-
-
-
-
-
-
-
-
+const handleVote = ()=>{
+  const copyOfVotes = [...votes]
+  copyOfVotes[selected] += 1
+  setVotes(copyOfVotes)
+}
   return (
+    
     <>
-    <div>
-      <h1>Give Feedback</h1>
+    {anecdotes[selected]}
+    <br />
+    <button onClick={handleVote}>Vote</button>
 
-      <Button incrementar={goodIn} name={"Good"}></Button>
-      <Button incrementar={neutralIn}  name={"Neural"}></Button>
-      <Button incrementar={badIn} name={"Bad"}></Button>
-
-      <h2>Statistics</h2>
-
-      <table>
-
-
-      <Statistic name={"Good"} props={good} ></Statistic>
-      <Statistic name={"Neutral"} props={neutral}  ></Statistic>
-      <Statistic name={"Bad"} props={bad}  ></Statistic>
-      <Statistic name={"All"}  props={all} ></Statistic>
-      <Statistic name={"average"}  props={promedio} ></Statistic>
-      <Statistic name={"positive"}  props={promedioPositivo} ></Statistic>
-      </table>
-    </div>
+    <button onClick={nextAnecdote}>next anectdote</button>
+    <br />
+    <h2>votes {votes[selected]}</h2>
     </>
 
   )
