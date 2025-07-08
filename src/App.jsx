e3012a20a36cdc16e1624012edd654a3051bf3c6
Adolfois3 <adolfito241107@gmail.com>
2025-07-08 15:56:18 -0400

    ejercicio 1.9 realizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,83 @@
+import { useState } from "react"
 
 
+const Statistic = ({props, name})=>{
+
+  return(
+    <>
+    <p>{name} {props}</p>
+    </>
+  )
+
+}
+
+const Button = ({name,incrementar})=>{
+
+  return(
+    <button onClick={incrementar}>{name}</button>
+
+  )
+}
 
 export const App =  () => {
 
-const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+
+
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all , setAll]= useState(0)
+
+  function goodIn (){
+    setGood(good + 1)
+        setAll(all + 1)
   }
+    function neutralIn (){
+    setNeutral(neutral + 1)
+        setAll(all + 1)
+  }
+
+    function badIn (){
+    setBad(bad + 1)
+    setAll(all  + 1 )
+  }
+
+
+    const promedio = good + neutral + bad / all
+    const promedioPositivo = good / all * 100
+
+
+
+
 
 
 
 
   return (
+    <>
     <div>
-      <h1>{course.name}</h1>
+      <h1>Give Feedback</h1>
 
-      {course.parts.map((params)=>{
-        return(
-          <>
-          <p>{params.name}</p>
-          <p>{params.exercises}</p>
-          </>
-        )
+      <Button incrementar={goodIn} name={"Good"}></Button>
+      <Button incrementar={neutralIn}  name={"Neural"}></Button>
+      <Button incrementar={badIn} name={"Bad"}></Button>
 
-      })}
+      <h2>Statistics</h2>
+
+      <table>
 
 
+      <Statistic name={"Good"} props={good} ></Statistic>
+      <Statistic name={"Neutral"} props={neutral}  ></Statistic>
+      <Statistic name={"Bad"} props={bad}  ></Statistic>
+      <Statistic name={"All"}  props={all} ></Statistic>
+      <Statistic name={"average"}  props={promedio} ></Statistic>
+      <Statistic name={"positive"}  props={promedioPositivo} ></Statistic>
+      </table>
     </div>
+    </>
+
   )
 
 }
